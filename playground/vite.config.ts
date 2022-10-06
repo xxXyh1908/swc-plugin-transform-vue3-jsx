a7f5a4965093f79fcee3b02ab0414716a0ca6b30
xxXyh1908 <15626874924@163.com>
2022-10-06 14:24:28 +0800

    v0.2.0

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -3,10 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@assets': './src/assets'
-    }
-  },
+  build: { minify: 'esbuild' },
   plugins: [vue()]
 })
